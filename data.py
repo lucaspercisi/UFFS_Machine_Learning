0bec193a24a99cc168aea8a2f284083d00f41e01
Lucas Percisi <lucas_percisi@hotmail.com>
2019-03-15 22:36:14 -0300

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 #carrega sem o cabeçalho, sem as duas primeirasa colunas e sem as 2 ultimas colunas
 data = np.loadtxt("AirQualityUCI.csv", dtype=str, delimiter=';',skiprows=1)[:,2:-2] 
@@ -6,4 +7,15 @@ data = np.loadtxt("AirQualityUCI.csv", dtype=str, delimiter=';',skiprows=1)[:,2:
 #exclui todas linhas vazias do final do arquivo
 data = data[:np.nonzero(data=='')[0][0],:]
 
-print (data)
+#converte toda a tabela em float substituindo virgulas por pontos.
+data = np.char.replace(data,',','.').astype(np.float)
+
+#salva 4ª coluna
+y = data[:,3]
+
+#deleta 4ª coluna do data
+data = np.delete(data, 3, axis=1)
+
+x = pd.DataFrame(data)
+x = x.interpolate(data)
+x = np.array(x)
